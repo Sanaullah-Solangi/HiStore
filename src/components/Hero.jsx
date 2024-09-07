1957b062288efd080bb88872e7b7598db8dc11de
Sanaullah <sanaullahsolangi817@gmail.com>
2024-09-07 18:13:25 +0500

    Replaced background images in the Hero page's main container with an <img> tag for better image handling and responsiveness.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
+import HeroImg from "../assets/images/sample-3.jpg";
 function Hero() {
   return (
-    <section className="heroContainer w-screen h-screen text-gray-600 body-font">
+    <section className="heroContainer w-screen text-gray-600 body-font">
+      <img src={HeroImg} className="h-full object-contain" alt="" />
       {/* <div className=" container  "></div> */}
     </section>
   );
